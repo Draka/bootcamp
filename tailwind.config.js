9bc5f90320fd2537b9147d913d07f84b812d1212
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2024-02-16 14:41:09 -0600

    Nuevas pantallas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        acviolet: {
+          300: "#A100FF",
+        },
+        acbg: "#F2F0FF",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
